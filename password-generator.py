58405e48073b7afa2239be8271cb754328cbe22a
Jorge Alejandro Acosta Bravo <31253418+jalcobra@users.noreply.github.com>
2023-05-28 07:15:40 -0500

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,5 +1,3 @@
-#Password Generator Project
-#Made by Jorge Alejandro Acosta Bravo (Jalcobra)
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -23,10 +21,8 @@ for n in range(0, nr_symbols):
 for n in range(0, nr_numbers):
   password_list.append(numbers[random.randint(0,len(numbers)) -1])
   
-print(password_list)
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 random.shuffle(password_list)
-print(password_list)
 password = password.join(password_list)
 print(f"Your password is: {password}")
